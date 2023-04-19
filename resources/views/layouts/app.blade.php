fabb007f149baf336239890b5617a93e72f3143c
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-04-19 09:49:04 +0700

    edit margin footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,13 +36,6 @@
 </head>
 <body>
     <div class="container-xxl bg-white p-0">
-        <!-- Spinner Start -->
-        <!-- <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div> -->
-        <!-- Spinner End -->
 
         <!-- Navbar & Hero Start -->
         <div class="container-xxl position-relative p-0">
@@ -86,7 +79,7 @@
         @yield('content')
 
         <!-- Footer Start -->
-        <div class="container-fluid bg-primary text-light footer wow fadeIn" data-wow-delay="0.1s" style="margin-top: 6rem;">
+        <div class="container-fluid bg-primary text-light footer wow fadeIn" data-wow-delay="0.1s">
             <div class="container">
                 <div class="copyright">
                     <div class="row">
