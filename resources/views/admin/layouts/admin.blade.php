0fd90157677bdb9e52f97a226c85c396b191475f
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-05-15 11:32:22 +0700

    fix symlink filesystem

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -182,7 +182,8 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/storage/app/img/{{ Session::get('photo') }}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('/storage/app/img/'. Session::get('photo')) }}" class="img-circle elevation-2" alt="Image">
+          {{-- <img src="/storage/app/img/{{ Session::get('photo') }}" class="img-circle elevation-2" alt="Image"> --}}
         </div>
         <div class="info">
           <a href="/admin/user" class="d-block">{{ Session::get('name') }}
